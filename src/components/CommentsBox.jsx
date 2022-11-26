69ffca69e91f55ec4bd407ed5e749c088da9682d
gourav017 <gouravsahu696@gmail.com>
2022-11-26 15:52:08 +0530

    post get the data

--- src/components/CommentsBox.jsx
+++ src/components/CommentsBox.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Allcomments from "./Allcomments";
+import axios from "axios";
 
 const CommentsBox = () => {
   const [inputComment, setinputComment] = useState("");
-  const [comments, setcomments] = useState([])
+  const [comments, setcomments] = useState([]);
 
-  const handleCommentsubmit=()=>{
-      setcomments([...comments,{id:comments.length+1,comment:inputComment}])
+  useEffect(() => {
+    getallcommetns();
+  }, []);
 
-  }
+  const handleCommentsubmit = () => {
+    return axios
+      .post("http://localhost:8080/comments", { comments: inputComment })
+      .then(() => getallcommetns());
+  };
 
+  const getallcommetns = async () => {
+    return await axios
+      .get("http://localhost:8080/comments")
+      .then((res) => setcomments(res.data))
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div>
@@ -21,7 +33,9 @@ const CommentsBox = () => {
         onChange={(e) => setinputComment(e.target.value)}
       />
       <button onClick={handleCommentsubmit}>submit</button>
-      <Allcomments allcoments={comments} />
+      {comments.map((c) => (
+        <p>{c.comments}</p>
+      ))}
     </div>
   );
 };
